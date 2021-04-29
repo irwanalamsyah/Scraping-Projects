69f93c45d6bbadb80f51a91c03f661a9eaa8df03
Irwan Alamsyah <irwanalamsyah.dev@gmail.com>
2021-04-29 08:18:19 +0700

    Adding setup

--- DjangoScrapy_Movies/DjangoScrapy_Movies/settings.py
+++ DjangoScrapy_Movies/DjangoScrapy_Movies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qm8t*=3h4%5pn5xecowpjou!$u!wk!r&&1pw99-hzot5u0qemz'
+SECRET_KEY = 'django-insecure-mfw$@ap^hjrm*du*k*77hzwun=0p_t7tu!p7q%^%=8q8l01ggs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +123,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Media files Django 3.1
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'static/media'
--- DjangoScrapy_Movies/README.md
+++ DjangoScrapy_Movies/README.md
@@ -1,1 +1,29 @@
-## Scraping using Django + Scrapy 
+# Scraping Django + Scrapy
+### Versions:
+`Python 3.8.2 `
+`Django 3.2`
+`Scrapy 2.5.0`
+
+### Let's collect the best movies from 2020
+* Supposing that you already have python in your computer.
+
+### Setup
+1 - Install requirements
+````
+$ pip install -r requirements.txt
+````
+2 - Configure the database
+````
+$ python manage.py migrate
+````
+
+3. Install django 3.8.2
+```
+$ pip install Django==3.8.2
+```
+
+4. Create app Movie
+```
+$ python manage.py startapp movie
+```
+
